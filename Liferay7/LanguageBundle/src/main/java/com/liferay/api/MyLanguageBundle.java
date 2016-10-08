e78a1aa1588e7106d0b482031231eae6536eede8
Danish12 <mohddanish27744@gmail.com>
2016-10-08 16:14:51 +0530

    Add files via upload

---
+++ Liferay7/LanguageBundle/src/main/java/com/liferay/api/MyLanguageBundle.java
@@ -0,0 +1,25 @@
+package com.liferay.api;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.liferay.portal.kernel.language.UTF8Control;
+
+@Component(property = { "language.id=en_US" }, service = ResourceBundle.class)
+public class MyLanguageBundle extends ResourceBundle {
+
+	@Override
+	protected Object handleGetObject(String key) {
+		return _resourceBundle.getObject(key);
+	}
+
+	@Override
+	public Enumeration<String> getKeys() {
+		return _resourceBundle.getKeys();
+	}
+
+	private final ResourceBundle _resourceBundle = ResourceBundle.getBundle("content.Language", UTF8Control.INSTANCE);
+
+}
